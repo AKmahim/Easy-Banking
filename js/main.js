ae60f48546ab71418ccc7279a12084e713784299
AKmahim <mdmahimhossen134@gmail.com>
2021-08-20 22:18:36 +0600

    Easy Banking

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// var icon = document.getElementById('nav-toggle')
+
+
+
+// // function myFunction() {
+// //     var x = document.getElementById("nav-list");
+// //     if (x.style.display === "block") {
+// //       x.style.display = "none";
+// //     } else {
+// //       x.style.display = "block";
+// //     }
+// //   }
+
+//   icon.addEventListener('click',()=>{
+//     var x = document.getElementById("nav-list");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   });
+
+var navList = document.getElementById("nav-list");
+navList.style.top = "5rem";
